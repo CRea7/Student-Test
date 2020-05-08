fbbff694858eb84b4d9a539ecf24ac59ace8b289
CRea7 <conorrea123@gmail.com>
2020-05-08 21:12:58 +0100

    initial commit

---
+++ build/generated-sources/ap-source-output/enttities/Students_.java
@@ -0,0 +1,16 @@
+package enttities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-08T14:59:03")
+@StaticMetamodel(Students.class)
+public class Students_ { 
+
+    public static volatile SingularAttribute<Students, String> fname;
+    public static volatile SingularAttribute<Students, String> sname;
+    public static volatile SingularAttribute<Students, String> course;
+    public static volatile SingularAttribute<Students, Integer> id;
+
+}
---
+++ src/java/enttities/Students.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enttities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author conor
+ */
+@Entity
+@Table(name = "students")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Students.findAll", query = "SELECT s FROM Students s")
+    , @NamedQuery(name = "Students.findById", query = "SELECT s FROM Students s WHERE s.id = :id")
+    , @NamedQuery(name = "Students.findByFname", query = "SELECT s FROM Students s WHERE s.fname = :fname")
+    , @NamedQuery(name = "Students.findBySname", query = "SELECT s FROM Students s WHERE s.sname = :sname")
+    , @NamedQuery(name = "Students.findByCourse", query = "SELECT s FROM Students s WHERE s.course = :course")})
+public class Students implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "FNAME")
+    private String fname;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "SNAME")
+    private String sname;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "COURSE")
+    private String course;
+
+    public Students() {
+    }
+
+    public Students(Integer id) {
+        this.id = id;
+    }
+
+    public Students(Integer id, String fname, String sname, String course) {
+        this.id = id;
+        this.fname = fname;
+        this.sname = sname;
+        this.course = course;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Students)) {
+            return false;
+        }
+        Students other = (Students) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "enttities.Students[ id=" + id + " ]";
+    }
+    
+}
